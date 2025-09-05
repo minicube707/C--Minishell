33a19b5cc97706a0b931409af52daf3d04d5ebab
Florent <florentm.contact@gmail.com>
2025-09-05 13:22:21 +0200

    (Chore): Commun - Update header

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lupayet <lupayet@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 17:18:58 by fmotte            #+#    #+#             */
-/*   Updated: 2025/09/04 18:37:29 by fmotte           ###   ########.fr       */
+/*   Updated: 2025/09/05 13:20:01 by fmotte           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,33 @@
 # define MINISHELL_H
 
 # include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/stat.h>
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <unistd.h>
-
 // STRUCTURE
+
+// Structure to containt the file info
+typedef struct s_file_info
+{
+	int				type;
+	char			*file_name;
+	int				fd;
+}					t_file_info;
+
+// Strucuture to containt the environnement
+typedef struct s_list_env
+{
+	int				type;
+	char			*file_name;
+	int				fd;
+}					t_list_env;
+
+// Strucuture to containt the info of the commande line
 typedef struct s_list
 {
 	t_list_env		**enviroment;
@@ -33,21 +52,5 @@ typedef struct s_list
 	struct s_list	*next;
 	struct s_list	*previous;
 }					t_list;
-
-typedef struct s_file_info
-{
-	int				type;
-	char			*file_name;
-	int				fd;
-}					t_file_info;
-
-typedef struct s_list_env
-{
-	int				type;
-	char			*file_name;
-	int				fd;
-}					t_list_env;
-
-
 
 #endif
