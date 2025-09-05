28462b7ce5be0e265e849bfbbbc078385e0a4efc
lupayet <lupayet@student.42.fr>
2025-09-05 16:37:59 +0200

    (CHORE): minor fix norm

---
+++ src/parser.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lupayet <lupayet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/05 15:05:15 by lupayet           #+#    #+#             */
+/*   Updated: 2025/09/05 16:36:03 by lupayet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "minishell.h"
+
+# define LT <
+# define GT >
+# define LOET <<
+# define GOET >>
+# define AND &&
+# define OR ||
+# define PIPE |
+
+t_list	*parser(int argc, char **argv, char **envp)
+{
+		
+}
