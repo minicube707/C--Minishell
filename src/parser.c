ffdd1583d97833bbe2a33d98fc0917722c8f2de4
lupayet <lupayet@student.42.fr>
2025-09-07 19:07:49 +0200

    Env in progress

--- src/parser.c
+++ src/parser.c
@@ -21,7 +21,61 @@
 # define OR ||
 # define PIPE |
 
+int	strlenc(char *str, const char c)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (-1);
+	while (*str++ && *str != c)
+		i++;
+	return (i);
+}
+
+t_list_env	*set_env(char **envp)
+{
+	t_list_env	*new;
+	t_list_env	*tmp;
+	int		len;
+	int		i;
+
+	new = NULL;
+	while (*envp)
+	{
+		len = strlenc(*envp, '=');
+		if (!new)
+			new = malloc(sizeof(t_list_env));
+		else
+		{
+			new->next = malloc(sizeof(t_list_env));
+			tmp = new;
+			new = new->next;
+			new->prev = tmp;
+			new->next = NULL;
+		}
+		new->name = ft_substr(*envp, 0, len);
+		if (!new->name)
+			write(1, "Error env-name\n", 15);
+		new->content = getenv(new->name);
+		envp++;
+	}
+	return (new);
+}
+
 t_list	*parser(int argc, char **argv, char **envp)
 {
-		
+	t_list	*list;
+
+	list = malloc(sizeof(t_list));
+	if (!list)
+		return (NULL);
+	list->enviroment = set_env(envp);
+	t_list_env	p = *list->enviroment;
+	while (p)
+	{
+		ft_printf("%s = %s\n", p->name, p->content);
+		p = p->next;
+	}
+	return (list);
 }
