6c4717dbf4ec0ee159c371c91756d6e6036215c9
lupayet <lupayet@student.42.fr>
2025-09-30 15:50:33 +0200

    tmp

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 18:16:22 by lupayet           #+#    #+#             */
-/*   Updated: 2025/09/30 10:55:27 by lupayet          ###   ########.fr       */
+/*   Updated: 2025/09/30 15:40:33 by lupayet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,9 +94,9 @@ void	print_list(t_list *head)
 			printf("  options  : (null)\n");
 		}
 		print_file_info(head->tab_file);
-		//		print_channel(head->in_out);
-		//		printf("  subshell : %s\n",
-	//				head->subshell ? head->subshell : "(null)");
+//				print_channel(head->in_out);
+				printf("  subshell : %s\n",
+					head->subshell ? head->subshell : "(null)");
 		head = head->next;
 		index++;
 	}
