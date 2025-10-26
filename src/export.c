a5d166f95fc1a1dd0e3ef76e8a98a01976425cee
lupayet <lupayet@student.42.fr>
2025-10-26 22:14:15 +0100

    tmp

--- src/export.c
+++ src/export.c
@@ -6,7 +6,7 @@
 /*   By: fmotte <fmotte@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 14:51:49 by lupayet           #+#    #+#             */
-/*   Updated: 2025/10/24 14:16:16 by fmotte           ###   ########.fr       */
+/*   Updated: 2025/10/24 19:03:46 by lupayet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,18 +33,6 @@ static int	error_id(char *arg)
 	ft_putstr_fd("' is not a valid identifier\n", 2);
 	return (1);
 }
-/*
-static void	set_new_env(t_list_env *new, char *arg)
-{
-	size_t	l;
-
-	l = strlenc(arg, '=');
-	new->name = ft_substr(arg, 0, l);
-	if (arg[l] == '=')
-		l++;
-	new->content = ft_strdup(&arg[l]);
-	new->next = NULL;
-}*/
 
 static void	sort_list(t_list_env **arr, int size)
 {
